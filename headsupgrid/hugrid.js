f1c20423089cdb42b2defc3672e2bdf6b2954fbd
Jason Simanek <jsimanek@gmail.com>
2012-05-24 23:42:24 -0500

    should fix issue #5

--- headsupgrid/hugrid.js
+++ headsupgrid/hugrid.js
@@ -3,11 +3,11 @@
 * Simple HTML + CSS grid overlay for web design and development.
 *
 * Files: hugrid.css, hugrid.js, example.html
-*
+* 
 * Example and documentation at: http://bohemianalps.com/tools/grid
-*
+* 
 * Shurane, thanks for your help! https://github.com/shurane
-*
+* 
 * Copyright (c) 2011 Jason Simanek
 *
 * Version: 1.5 (09/03/2011)
@@ -17,145 +17,111 @@
 *   http://www.gnu.org/licenses/gpl.html
 */
 
-// the semicolon at the beginning is there on purpose in order to protect the integrity
-// of your scripts when mixed with incomplete objects, arrays, etc.
-// TODO is it required?
-// ;
-(function ($) {
+;(function ($) {
   // "use strict";
+  makehugrid = function () {
+    /* Remove Previously Existing Grid Elements */
+    $('#hugrid').remove();
+    $('#hugridRows').remove();
+    $('#hugridUX').remove();
 
-  $.headsUpGrid = function(options) {
+    /* Column Container */
+    var hugridDiv = document.createElement("div");
+    hugridDiv.id  = "hugrid";
 
-    var defaults = {
-      pageUnits: 'px',
-      colUnits: 'px',
-      pageWidth: 960,
-      columns: 6,
-      columnWidth: 140,
-      gutterWidth: 24,
-      pageTopMargin: 35,
-      rowHeight: 20,
-      showGridOnload: false
-    };
+    /* Left Margin Column */
+    leftDiv = document.createElement("div");
+    leftDiv.className = "mline mlineL";
+    hugridDiv.appendChild(leftDiv);
 
-    // Avoid 'this' confusion
-    var plugin = this;
+    /* Create Columns */
+    for (var i = 0; i < (columns - 1); i++) {
+      colDiv = document.createElement("div");
+      colDiv.className = "hugcol";
+      hugridDiv.appendChild(colDiv);
+      lineLDiv = document.createElement("div");
+      lineLDiv.className = "lineL";
+      colDiv.appendChild(lineLDiv);
+      lineRDiv = document.createElement("div");
+      lineRDiv.className = "lineR";
+      colDiv.appendChild(lineRDiv);
+    }
 
-    // This will hold the merged default, and user-provided options
-    // plugin's properties will be accessible like:
-    // plugin.settings.propertyName from inside the plugin or
-    // myplugin.settings.propertyName from outside the plugin
-    // where "myplugin" is an instance of the plugin
-    plugin.settings = {};
+    /* Right Margin Column */
+    rightDiv = document.createElement("div");
+    rightDiv.className = "mline mlineR";
+    hugridDiv.appendChild(rightDiv);
 
-    var init = function() {
-      plugin.settings = $.extend({}, defaults, options);
-      plugin.makehugrid();
-    };
+    document.body.appendChild(hugridDiv);
 
-    plugin.makehugrid = function() {
-
-      /* Remove Previously Existing Grid Elements */
-      $('#hugrid').remove();
-      $('#hugridRows').remove();
-      $('#hugridUX').remove();
-
-      /* Column Container */
-      var hugridDiv = document.createElement("div");
-      hugridDiv.id  = "hugrid";
-
-      /* Left Margin Column */
-      leftDiv = document.createElement("div");
-      leftDiv.className = "mline mlineL";
-      hugridDiv.appendChild(leftDiv);
-
-      /* Create plugin.settings.columns */
-      for (var i = 0; i < (plugin.settings.columns - 1); i++) {
-        colDiv = document.createElement("div");
-        colDiv.className = "hugcol";
-        hugridDiv.appendChild(colDiv);
-        lineLDiv = document.createElement("div");
-        lineLDiv.className = "lineL";
-        colDiv.appendChild(lineLDiv);
-        lineRDiv = document.createElement("div");
-        lineRDiv.className = "lineR";
-        colDiv.appendChild(lineRDiv);
+    /* If Rows */
+    if (rowheight != 0)  {
+      /* Row Container */
+      pageheight     = $(document.body).height();
+      var hugridRows = document.createElement("div")
+      hugridRows.id  = "hugridRows";
+      /* Create Rows */
+      for (var i = 0; i < (pageheight / rowheight); i++) {
+        rowDiv = document.createElement("div");
+        rowDiv.className = "hugrow";
+        hugridRows.appendChild(rowDiv);
+        lineB = document.createElement("div");
+        lineB.className = "lineB";
+        rowDiv.appendChild(lineB);
       }
 
-      /* Right Margin Column */
-      rightDiv = document.createElement("div");
-      rightDiv.className = "mline mlineR";
-      hugridDiv.appendChild(rightDiv);
+      document.body.appendChild(hugridRows);
+    }
 
-      document.body.appendChild(hugridDiv);
+    /* Apply CSS Properties */
+    $('#hugrid').css('width', pagewidth + pageUnits);
 
-      /* If Rows */
-      if (plugin.settings.rowHeight !== 0)  {
-        /* Row Container */
-        pageheight     = $(document.body).height();
-        var hugridRows = document.createElement("div");
-        hugridRows.id  = "hugridRows";
-        /* Create Rows */
-        for (var j = 0; j < (pageheight / plugin.settings.rowHeight); j++) {
-          rowDiv = document.createElement("div");
-          rowDiv.className = "hugrow";
-          hugridRows.appendChild(rowDiv);
-          lineB = document.createElement("div");
-          lineB.className = "lineB";
-          rowDiv.appendChild(lineB);
-        }
-
-        document.body.appendChild(hugridRows);
-      }
-
-      /* Apply CSS Properties */
-      $('#hugrid').css('width', plugin.settings.pageWidth + plugin.settings.pageUnits);
-
-      if (typeof window.pageleftmargin === 'number') {
-        $('#hugrid').css('left', pageleftmargin + plugin.settings.pageUnits);
-        $('#hugrid').css('margin', '0');
-      } else if (typeof window.pagerightmargin === 'number') {
-        $('#hugrid').css('right', pagerightmargin + plugin.settings.pageUnits);
-        $('#hugrid').css('left', 'auto');
-        $('#hugrid').css('margin', '0');
+    if (typeof window.pageleftmargin === 'number') {
+      $('#hugrid').css('left', pageleftmargin + pageUnits);
+      $('#hugrid').css('margin', '0');
+    } else if (typeof window.pagerightmargin === 'number') {
+      $('#hugrid').css('right', pagerightmargin + pageUnits);
+      $('#hugrid').css('left', 'auto');
+      $('#hugrid').css('margin', '0');
+    } else {
+      if (pageUnits === '%') {
+        $('#hugrid').css('left', ((100 - pagewidth) / 2) + pageUnits);
+        $('#hugrid').css('margin-left', 'auto');
       } else {
-        if (plugin.settings.pageUnits === '%') {
-          $('#hugrid').css('left', ((100 - plugin.settings.pageWidth) / 2) + plugin.settings.pageUnits);
-          $('#hugrid').css('margin-left', 'auto');
-        } else {
-          $('#hugrid').css('margin-left', '-' + (plugin.settings.pageWidth / 2) + plugin.settings.pageUnits);
-        }
+        $('#hugrid').css('margin-left', '-' + (pagewidth / 2) + pageUnits);
       }
+    }
 
-      $('#hugrid div.hugcol').css('margin-left', plugin.settings.columnWidth + plugin.settings.colUnits);
-      $('#hugrid div.hugcol').css('width', plugin.settings.gutterWidth + plugin.settings.colUnits);
-      $('#hugridRows').css('margin-top', plugin.settings.pageTopMargin + 'px');
-      $('#hugridRows div.hugrow').css('margin-top', (plugin.settings.rowHeight - 1) + 'px');
+    $('#hugrid div.hugcol').css('margin-left', columnwidth + colUnits);
+    $('#hugrid div.hugcol').css('width', gutterwidth + colUnits);
+    $('#hugridRows').css('margin-top', pagetopmargin + 'px');
+    $('#hugridRows div.hugrow').css('margin-top', (rowheight - 1) + 'px');
 
-      /* Create hugridUX and button */
-      var hugridUX = document.createElement("div");
-      hugridUX.id = "hugridUX";
-      document.body.appendChild(hugridUX);
-      $('#hugridUX').append('<div id="hugridButtonBkgd"></div><button id="hugridButton"></button>');
-      $('#hugridButton').append('<span id="hugbuttonON">ON</span>');
-      $('#hugridButton').append('<span id="hugbuttonOFF" style="display:none;">OFF</span>');
+    /* Create hugridUX and button */
+    var hugridUX = document.createElement("div");
+    hugridUX.id = "hugridUX";
+    document.body.appendChild(hugridUX);
+    $('#hugridUX').append('<div id="hugridButtonBkgd"></div><button id="hugridButton"></button>');
+    $('#hugridButton').append('<span id="hugbuttonON">ON</span>');
+    $('#hugridButton').append('<span id="hugbuttonOFF" style="display:none;">OFF</span>');
 
-      /* On/Off Button - click functionality */
-      $('#hugridButton').click(function () {
-        $('#hugridButton').toggleClass('buttonisoff');
-        $('#hugrid').toggle();
-        $('#hugridRows').toggle();
-        $("#hugridButton span").toggle();
-      });
-
-      if (plugin.settings.showGridOnload === false) {
-        $('#hugridButton').toggleClass('buttonisoff');
-        $('#hugrid').toggle();
-        $('#hugridRows').toggle();
-        $("#hugridButton span").toggle();
-      }
-    };
-
-    init();
+    /* On/Off Button - click functionality */
+    $('#hugridButton').click(function () {
+      $('#hugridButton').toggleClass('buttonisoff')
+      $('#hugrid').toggle();
+      $('#hugridRows').toggle();
+      $("#hugridButton span").toggle();
+    });
   };
+
+  setgridonload = function () {
+    /* Default On/Off Setting */
+    if (gridonload === 'off') {
+      $('#hugridButton').toggleClass('buttonisoff')
+      $('#hugrid').toggle();
+      $('#hugridRows').toggle();
+      $("#hugridButton span").toggle();
+    }
+  };
+
 })(jQuery);
